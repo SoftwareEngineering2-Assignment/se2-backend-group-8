5a2379526446f220bbd58b2def45469b53ceff2e
Emkot87 <emkot87@live.com>
2022-12-10 15:48:15 +0200

    Update init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -1,30 +1,30 @@
-// /* eslint-disable import/no-unresolved */
-// require('dotenv').config();
+/* eslint-disable import/no-unresolved */
+require('dotenv').config();
 
-// const http = require('node:http');
-// const test = require('ava').default;
-// const got = require('got');
-// const listen = require('test-listen');
+const http = require('node:http');
+const test = require('ava').default;
+const got = require('got');
+const listen = require('test-listen');
 
-// const app = require('../src/index');
-// const {jwtSign} = require('../src/utilities/authentication/helpers');
+const app = require('../src/index');
+const {jwtSign} = require('../src/utilities/authentication/helpers');
 
-// test.before(async (t) => {
-//   t.context.server = http.createServer(app);
-//   t.context.prefixUrl = await listen(t.context.server);
-//   t.context.got = got.extend({http2: true, throwHttpErrors: false, responseType: 'json', prefixUrl: t.context.prefixUrl});
-// });
+test.before(async (t) => {
+  t.context.server = http.createServer(app);
+  t.context.prefixUrl = await listen(t.context.server);
+  t.context.got = got.extend({http2: true, throwHttpErrors: false, responseType: 'json', prefixUrl: t.context.prefixUrl});
+});
 
-// test.after.always((t) => {
-//   t.context.server.close();
-// });
+test.after.always((t) => {
+  t.context.server.close();
+});
 
-// test('GET /statistics returns correct response and status code', async (t) => {
-//   const {body, statusCode} = await t.context.got('general/statistics');
-//   t.is(body.sources, 1);
-//   t.assert(body.success);
-//   t.is(statusCode, 200);
-// });
+test('GET /statistics returns correct response and status code', async (t) => {
+  const {body, statusCode} = await t.context.got('general/statistics');
+  t.is(body.sources, 1);
+  t.assert(body.success);
+  t.is(statusCode, 200);
+});
 
 // test('GET /sources returns correct response and status code', async (t) => {
 //   const token = jwtSign({id: 1});
@@ -33,21 +33,23 @@
 // });
 
 /* eslint-disable import/no-unresolved */
-const test = require('ava').default;
 
-test('Test to pass', (t) => {
-  t.pass();
-});
 
-test('Test value', async (t) => {
-  const a = 1;
-  t.is(a + 1, 2);
-});
+// const test = require('ava').default;
 
-const sum = (a, b) => a + b;
+// test('Test to pass', (t) => {
+//   t.pass();
+// });
 
-test('Sum of 2 numbers', (t) => {
-  t.plan(2);
-  t.pass('this assertion passed');
-  t.is(sum(1, 2), 3);
-});
+// test('Test value', async (t) => {
+//   const a = 1;
+//   t.is(a + 1, 2);
+// });
+
+// const sum = (a, b) => a + b;
+
+// test('Sum of 2 numbers', (t) => {
+//   t.plan(2);
+//   t.pass('this assertion passed');
+//   t.is(sum(1, 2), 3);
+// });
