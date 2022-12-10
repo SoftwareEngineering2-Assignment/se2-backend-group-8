4471380d911e606a76225faff1d00c4dbf025324
Emkot87 <emkot87@live.com>
2022-12-10 15:54:25 +0200

    Update init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -21,35 +21,14 @@ test.after.always((t) => {
 
 test('GET /statistics returns correct response and status code', async (t) => {
   const {body, statusCode} = await t.context.got('general/statistics');
-  t.is(body.sources, 1);
+  t.is(body.sources, 0);
   t.assert(body.success);
   t.is(statusCode, 200);
 });
 
-// test('GET /sources returns correct response and status code', async (t) => {
-//   const token = jwtSign({id: 1});
-//   const {statusCode} = await t.context.got(`sources/sources?token=${token}`);
-//   t.is(statusCode, 200);
-// });
+test('GET /sources returns correct response and status code', async (t) => {
+  const token = jwtSign({id: 1});
+  const {statusCode} = await t.context.got(`sources/sources?token=${token}`);
+  t.is(statusCode, 200);
+});
 
-/* eslint-disable import/no-unresolved */
-
-
-// const test = require('ava').default;
-
-// test('Test to pass', (t) => {
-//   t.pass();
-// });
-
-// test('Test value', async (t) => {
-//   const a = 1;
-//   t.is(a + 1, 2);
-// });
-
-// const sum = (a, b) => a + b;
-
-// test('Sum of 2 numbers', (t) => {
-//   t.plan(2);
-//   t.pass('this assertion passed');
-//   t.is(sum(1, 2), 3);
-// });
