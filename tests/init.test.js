58d9144137ceef0a5530f694c900c8674240208b
stavrosb00 <stavroscallmaster@yahoo.gr>
2022-12-29 11:36:37 +0200

    Update init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -58,13 +58,31 @@ test('GET /test-url returns 500 status and active: false on error', async (t, do
   sinon.restore();
 });
 
-// GET test URL request GET,POST,PUT
+// GET test URL request GET,POST,PUT 
 test('GET /test-url-request Sends GET request and returns response', async (t) => {
   // Send an HTTP GET request to the /test-url-request route
   const { statusCode } = await t.context.got.get(
-    'general/test-url-request?url=https://www.google.gr&type=GET'
+    'general/test-url-request?url=https://www.youtube.com&type=GET'
   );
+  // Check if the return status is 200
+  t.is(statusCode, 200);
 
+  // Send an HTTP POST request to the /test-url-request route
+  const { statusCode } = await t.context.got.get(
+    'general/test-url-request?url=https://www.youtube.com&type=POST'
+  );
+  // Check if the return status is 200
+  t.is(statusCode, 200);
+
+  // Send an HTTP PUT request to the /test-url-request route
+  const { statusCode } = await t.context.got.get(
+    'general/test-url-request?url=https://www.youtube.com&type=PUT'
+  );
+  // Check if the return status is 200
+  t.is(statusCode, 200);
+
+
+});
 
 
 
