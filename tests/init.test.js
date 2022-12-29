71c60092be5163fe62cd158da9e9dab0d3262b03
stavrosb00 <stavroscallmaster@yahoo.gr>
2022-12-29 11:05:47 +0200

    Update init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -36,11 +36,22 @@ test('GET /sources returns correct response and status code', async (t) => {
 
 // GET testing URL
 test('GET /test-url returns correct status code', async (t) => {
-  const { body, statusCode } = await t.context.got(
-    '/general/test-url?url=https://www.youtube.com'
-  );
+  const { body, statusCode } = await t.context.got('/general/test-url?url=https://www.youtube.com');
   // Check if the return status is 200
   t.is(statusCode, 200);
+});
+
+// GET testing URL for error
+test('GET /test-url Returns 500 status and active: false on error', async (t, done) => {
+  // Creating a stub method  of got,'get'
+  const gotStub = sinon
+    .stub(got, 'get')
+    .throws(new Error('Something went wrong'));
+  // Our method rejects on upcoming error
+  const { body, statusCode } = await t.context.got('general/test-url?url=http://testinganexceptionalurl35');
+  t.is(body.status, 500);
+  t.is(body.active, false);
+  sinon.restore();
 });
 
 // na dw gia ta / sta urls
